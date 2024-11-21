99e85947578c32cab46ebf860a6be2e931a8bb00
GyoniA <100374553+GyoniA@users.noreply.github.com>
2024-11-21 15:03:16 +0100

    Add data and staging table sql

---
+++ 1StagingTableCreation.sql
@@ -0,0 +1,40 @@
+CREATE TABLE UN_Indicators_Staging (
+    Country NVARCHAR(255),
+    Year INT,
+	TotalPopulationJul FLOAT, -- Total population as of July 1st
+    PopulationDensityJul FLOAT, -- Population density as of July 1st
+    SexRatioJul FLOAT, -- Sex ratio as of July 1st (males/100 females)
+    MedianAgeJul FLOAT, -- Median age as of July 1st
+    NetReproductionRate FLOAT, -- Net reproduction rate (surviving daughters/ woman)
+    LifeExpectancy FLOAT -- Life expectancy at birth (both sexes)
+);
+
+CREATE TABLE WB_School_Enrollment_Staging (
+    Country NVARCHAR(255),
+    Year INT,
+    School_Enrollment_Percent FLOAT -- Primary school enrollment (% net)
+);
+
+CREATE TABLE WB_GDP_Staging (
+    Country NVARCHAR(255),
+    Year INT,
+    GDP FLOAT, -- Gross domestic product (current US$)
+);
+
+CREATE TABLE WB_GNI_Staging (
+    Country NVARCHAR(255),
+    Year INT,
+    GNI FLOAT -- Gross national income (Atlas method)
+);
+
+CREATE TABLE WB_Global_Savings_Staging (
+    Country NVARCHAR(255),
+    Year INT,
+    Global_Savings FLOAT -- Gross savings % of GDP
+);
+
+CREATE TABLE WB_Global_PPP_Staging (
+    Country NVARCHAR(255),
+    Year INT,
+    Global_PPP FLOAT -- Global PPP conversion factor
+);
--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # BICountryProgressComparison
 Business Intelligence project, that compares the progress of countries and their health/education indicators
+
+# Data sources
+- https://population.un.org/wpp/Download/Standard/MostUsed/ (Compact version)
+- https://data.worldbank.org/indicator/SE.PRM.NENR?view=chart (Education)
+- https://data.worldbank.org/indicator/NY.GDP.MKTP.CD?view=chart (GDP)
+- https://data.worldbank.org/indicator/NY.GNP.ATLS.CD?view=chart (GNI Atlas method)
+- https://data.worldbank.org/indicator/NY.GNS.ICTR.ZS?view=chart (Gross savings % of GDP)
+- https://data.worldbank.org/indicator/PA.NUS.PPP?view=chart (Global PPP conversion factor)
